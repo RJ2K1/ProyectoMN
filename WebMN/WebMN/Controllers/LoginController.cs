8ebad67ce9cf80bc0648e9609d97859f72b6a404
Eduardo Calvo Castillo <ecalvo@crnova.com>
2023-09-22 09:33:46 -0600

    Commit 0 del proyecto

---
+++ WebMN/WebMN/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMN.Models;
+
+namespace WebMN.Controllers
+{
+    public class LoginController : Controller
+    {
+        Usuario usuarioModel = new Usuario();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult IniciarSesion()
+        {
+            return View();
+        }        
+
+    }
+}
---
+++ WebMN/WebMN/Models/Usuario.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebMN.Models
+{
+    public class Usuario
+    {
+
+
+    }
+}
