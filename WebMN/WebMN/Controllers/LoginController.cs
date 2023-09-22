23910cc2ae93c0b48ed5284126eeafee9bed1369
Eduardo Calvo Castillo <ecalvo@crnova.com>
2023-09-22 10:55:30 -0600

    Implementación de Razor y modelos

--- WebMN/WebMN/Controllers/LoginController.cs
+++ WebMN/WebMN/Controllers/LoginController.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebMN.Entities;
 using WebMN.Models;
 
 namespace WebMN.Controllers
 {
     public class LoginController : Controller
     {
-        //Esto es una instancia del modelo
         UsuarioModel usuarioModel = new UsuarioModel();
 
         public ActionResult Index()
@@ -17,20 +17,47 @@ namespace WebMN.Controllers
             return View();
         }
 
+
+        [HttpGet]
         public ActionResult IniciarSesion()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult IniciarSesion(UsuarioEnt entidad)
+        {
+            usuarioModel.IniciarSesion(entidad);
+            return View();
+        }
+
+
+        [HttpGet]
         public ActionResult RegistrarCuenta()
         {
             return View();
         }
 
+        [HttpPost]
+        public ActionResult RegistrarCuenta(UsuarioEnt entidad)
+        {
+            usuarioModel.RegistrarCuenta(entidad);
+            return View();
+        }
+
+
+        [HttpGet]
         public ActionResult RecuperarCuenta()
         {
             return View();
-        }        
+        }
+
+        [HttpPost]
+        public ActionResult RecuperarCuenta(UsuarioEnt entidad)
+        {
+            usuarioModel.RecuperarCuenta(entidad);
+            return View();
+        }
 
     }
 }
--- WebMN/WebMN/Models/UsuarioModel.cs
+++ WebMN/WebMN/Models/UsuarioModel.cs
@@ -2,25 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using WebMN.Entities;
 
 namespace WebMN.Models
 {
     public class UsuarioModel
     {
 
-        public void IniciarSesion()
+        public void IniciarSesion(UsuarioEnt entidad)
         { 
-        
+            //LLAMAR AL WEB API PARA VALIDAR EL USUARIO
         }
 
-        public void RegistrarCuenta()
+        public void RegistrarCuenta(UsuarioEnt entidad)
         {
-
+            //LLAMAR AL WEB API PARA REGISTRAR EL USUARIO
         }
 
-        public void RecuperarCuenta()
+        public void RecuperarCuenta(UsuarioEnt entidad)
         {
-
+            //LLAMAR AL WEB PARA VALIDAR EL USUARIO Y ENVIARLE UN CORREO
         }
 
     }
